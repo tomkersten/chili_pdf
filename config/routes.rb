59e70a7b89e931027be37753ff492140bb0fb6c5
Tom Kersten <tom@whitespur.com>
2011-05-20 18:35:17 -0500

    Fixed route to not stomp on existing export formts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:project_id/wiki/:id.:format', :controller => 'extended_wiki', :action => 'show', :requirements => {:format => /pdf/}
+  map.connect 'projects/:project_id/wiki/:id.pdf', :controller => 'extended_wiki', :action => 'show', :format => 'pdf'
 end
