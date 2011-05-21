92a3e7ac5babb7ecf74825083bbf4f82732fca15
Tom Kersten <tom@whitespur.com>
2011-05-20 22:42:32 -0500

    Ensure we don't cause Double Render error

--- app/controllers/extended_wiki_controller.rb
+++ app/controllers/extended_wiki_controller.rb
@@ -3,20 +3,22 @@ class ExtendedWikiController < WikiController
 
   def show
     super
-    respond_to do |format|
-      format.html {render :template => 'wiki/show'}
-      format.pdf {
-        render :pdf => "#{@project.name.underscore}_#{@page.title}",
-               :template => 'extended_wiki/show.pdf.html.erb',
-               :page_size => "Letter",
-               :user_style_sheet => "#{Rails.root}/public/plugin_assets/chili_pdf/stylesheets/pdf.css",
-               :margin => {
-                 :top    => "0.5in",
-                 :bottom => "0.5in",
-                 :left   => "0.5in",
-                 :right  => "0.5in"
-               }
-      }
+    unless performed? # Prevent double render
+      respond_to do |format|
+        format.html {render :template => 'wiki/show'}
+        format.pdf {
+          render :pdf => "#{@project.name.underscore}_#{@page.title}",
+                 :template => 'extended_wiki/show.pdf.html.erb',
+                 :page_size => "Letter",
+                 :user_style_sheet => "#{Rails.root}/public/plugin_assets/chili_pdf/stylesheets/pdf.css",
+                 :margin => {
+                   :top    => "0.5in",
+                   :bottom => "0.5in",
+                   :left   => "0.5in",
+                   :right  => "0.5in"
+                 }
+        }
+      end
     end
   end
 end
