7440b8f02ca399b48c296f5d363a720ab58ed4b5
Tom Kersten <tom@whitespur.com>
2011-06-06 23:48:34 -0500

    Added default header/footer content [refs #2]

--- app/controllers/extended_wiki_controller.rb
+++ app/controllers/extended_wiki_controller.rb
@@ -7,12 +7,16 @@ class ExtendedWikiController < WikiController
     super
     unless performed? # Prevent double render
       respond_to do |format|
-        format.pdf {render ChiliPDF::Formatter.render_options(filename)}
+        format.pdf {render pdf_render_options}
       end
     end
   end
 
   private
+    def pdf_render_options
+      ChiliPDF::Formatter.render_options(filename, @page.title)
+    end
+
     def filename
       "#{@project.name.underscore}_#{@page.title}".gsub(/\s/,'')
     end
--- lib/chili_pdf/formatter.rb
+++ lib/chili_pdf/formatter.rb
@@ -2,7 +2,9 @@ module ChiliPDF
   module Formatter
     extend self
 
-    def render_options(filename)
+    def render_options(filename, header_title = nil)
+      header_title ||= filename
+
       {
         :pdf => filename,
         :template => 'extended_wiki/show.pdf.html.erb',
@@ -13,8 +15,24 @@ module ChiliPDF
           :left => "0.5in",
           :right => "0.5in"
         },
+        :header => {
+          :font_size => 8,
+          :left => header_title,
+          :line => true
+        },
+        :footer => {
+          :font_size => 8,
+          :left => datestamp,
+          :right => '[page]/[topage]',
+          :line => true
+        },
         :layout => 'pdf.pdf.erb'
       }
     end
+
+    private
+      def datestamp
+        Time.now.strftime('%d-%b-%Y')
+      end
   end
 end
