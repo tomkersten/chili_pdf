1460888694ecdc72e12d1e1278d44cda218300d8
Tom Kersten <tom@whitespur.com>
2011-06-13 10:26:09 -0500

    Fixed require-order issue in chili_pdf.rb

--- lib/chili_pdf.rb
+++ lib/chili_pdf.rb
@@ -1,7 +1,7 @@
 require 'chili_pdf/config'
+require 'chili_pdf/token_manager'
 require 'chili_pdf/formatter'
 require 'chili_pdf/string_token'
-require 'chili_pdf/token_manager'
 
 module ChiliPDF
   VERSION = '0.2.0'
